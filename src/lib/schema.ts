4b5b3439896134bf717ce082b2ae65086f96572c
hket89 <hket89@gmail.com>
2021-03-24 00:57:24 +0800

    FIx schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -3,13 +3,13 @@ import { Comment } from '../types/comment';
 import { TopPostQuery } from '../types/post';
 
 export const filterCommentSchema: yup.SchemaOf<Comment> = yup.object({
-  postId: yup.number().nullable().notRequired(),
-  id: yup.number().notRequired(),
-  name: yup.string().nullable().notRequired(),
-  email: yup.string().nullable().notRequired().email(),
-  body: yup.string().nullable().notRequired()
+  postId: yup.number().default(0),
+  id: yup.number().default(0),
+  name: yup.string().default(''),
+  email: yup.string().default('').email(),
+  body: yup.string().default('')
 });
 
 export const topPostSchema: yup.SchemaOf<TopPostQuery> = yup.object({
-  limit: yup.number().nullable().notRequired().default(10)
+  limit: yup.number().notRequired().default(10)
 });
